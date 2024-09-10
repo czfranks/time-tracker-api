16d3817425de19f33fb1ac22d0c2a5eab7b9cab8
Franks <czfranks@gmail.com>
2024-09-10 08:52:31 -0500

    feat: Add show Users and create User on API

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import http from 'node:http';
+import { userRouter } from './routes/userRoutes';
 
-const requestListener: http.RequestListener = (_req, response) => {
-  response.end('Hello world!');
+const requestListener: http.RequestListener = (req, res) => {
+  const url = req.url || '/';
+  if (url == '/') {
+    res.writeHead(200, { 'content-type': 'text/plain' });
+    res.end('Welcome to time tracker api!');
+  } else if (url.startsWith('/users')) {
+    userRouter(req, res);
+  } else {
+    res.writeHead(404, { 'content-type': 'text/plain' });
+    res.end('Not found');
+  }
 };
 
 const server = http.createServer(requestListener);
-server.listen(5500);
+server.listen(5500, () => {
+  console.log('running in http://localhost:5500');
+});
